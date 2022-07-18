be7fc1f746c8ba020c152b4f19f2b12edf4f06b5
kazim <kazim@LAPTOP-BEMA2EP8>
2022-07-18 23:26:00 +0530

    Rick hello

--- src/demo/test.java
+++ src/demo/test.java
@@ -3,10 +3,10 @@ package demo;
 public class test {
  public static void main (String arg[]) {
 	 
- System.out.print("hi");
+ System.out.print("Hello Rick");
  int a=4;
  int b=3;
  int c=a+b;
- System.out.println("sum=" + c);
+ System.out.println(" "+"sum=" + c);
  }
 }
